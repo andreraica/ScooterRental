b70effbb3f293e9324849f1b74bf858c2f754132
Andre Raiça Silva <ext.andre.silva@farfetch.com>
2020-09-26 22:54:36 +0100

    Sending Tracking Message to Queue

---
+++ src/ScooterService/Domain.Services/DTO/TrackingMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace ScooterRental.Domain.Services.DTO
+{
+    public class TrackingMessage
+    {
+        public TrackingMessage(int scooterId, int locationId)
+        {
+            this.ScooterId = scooterId;
+            this.LocationId = locationId;
+        }
+
+        public int ScooterId { get; private set; }
+        public int LocationId { get; private set; }
+    }
+}
--- src/ScooterService/Domain.Services/ScooterService.cs
+++ src/ScooterService/Domain.Services/ScooterService.cs
@@ -1,17 +1,31 @@
 ﻿namespace ScooterRental.Domain.Services
 {
-    using System;
+    using Microsoft.Azure.ServiceBus;
+
+    using Newtonsoft.Json;
+
     using ScooterRental.Domain.Interfaces.Data.Repositories;
+    using ScooterRental.Domain.Interfaces.IoC;
     using ScooterRental.Domain.Interfaces.Services;
     using ScooterRental.Domain.Models;
 
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class ScooterService : IScooterService
     {
-        private readonly IScooterRepository scooterRepository;
+        const string ServiceBusConnectionString = "Endpoint=sb://scotterrentalqueue.servicebus.windows.net/;SharedAccessKeyName=SystemUser;SharedAccessKey=lmQRuxHRnlkm5iBJBJN8NJnmewtTAMqFrLo1JiYPoFw=;";
+        const string QueueName = "tracking";
+        static IQueueClient queueClient;
 
-        public ScooterService(IScooterRepository scooterRepository)
+        private readonly IScooterRepository scooterRepository;
+        private readonly ISettings settings;
+
+        public ScooterService(IScooterRepository scooterRepository, ISettings settings)
         {
             this.scooterRepository = scooterRepository;
+            this.settings = settings;
         }
 
         public Scooter Rent(int scooterId, string passportNumber)
@@ -20,12 +34,9 @@
             {
                 var scooter = this.scooterRepository.Get(scooterId);
                 scooter.Rent(passportNumber);
-
                 this.scooterRepository.Update(scooter);
 
-                //Monitor
-                //var locationAreaId = new Random().Next(1, 200);
-                //monitorDal.Insert(new Monitor(scooter, locationAreaId));
+                this.SendScooterLocation(scooterId);
 
                 return scooter;
             }
@@ -41,18 +52,47 @@
             {
                 var scooter = this.scooterRepository.Get(scooterId);
                 scooter.TurnBack();
-
                 this.scooterRepository.Update(scooter);
 
-                //Monitor
-                //var locationAreaId = new Random().Next(1, 200);
-                //monitorDal.Insert(new Monitor(scooter, locationAreaId));
+                this.SendScooterLocation(scooterId);
 
                 return scooter;
             }
             catch (ApplicationException appEx)
             {
                 throw appEx;
+            }
+        }
+
+        private void SendScooterLocation(int scooterId)
+        {
+            var trackingMessage = new DTO.TrackingMessage(
+                scooterId, new Random().Next(1, 200));
+
+            Task.Run(() => this.SendMessageAsync(trackingMessage));
+        }
+
+        private async Task SendMessageAsync(DTO.TrackingMessage trackingMessage)
+        {
+            try
+            {
+                queueClient = new QueueClient(
+                    this.settings.GetAppSetting("TrackingQueueConnectionString"),
+                    this.settings.GetAppSetting("TrackingQueueName"));
+
+                var message = new Message(
+                    Encoding.UTF8.GetBytes(
+                        JsonConvert.SerializeObject(trackingMessage).ToString()
+                        )
+                    );
+
+                await queueClient.SendAsync(message).ConfigureAwait(false);
+
+                await queueClient.CloseAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
         }
     }
